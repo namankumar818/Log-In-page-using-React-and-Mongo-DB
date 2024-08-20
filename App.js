1b3a9a2782d436aa1c4fe310fca03f59a81d3822
namankumar818 <80568028+namankumar818@users.noreply.github.com>
2024-08-20 14:20:33 +0530

    Update App.js

--- App.js
+++ App.js
@@ -22,5 +22,26 @@ function App() {
     </div>
   )
 }
+useEffect(() => {
+  
+  const user = JSON.parse(localStorage.getItem('user'))
 
+
+  if (!user || !user.token) {
+    setLoggedIn(false)
+    return
+  }
+  fetch('http://localhost:3080/verify', {
+    method: 'POST',
+    headers: {
+      'jwt-token': user.token,
+    },
+  })
+    .then((r) => r.json())
+    .then((r) => {
+      setLoggedIn('success' === r.message)
+      setEmail(user.email || '')
+    })
+}, [])
+ 
 export default App
