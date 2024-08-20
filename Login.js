1306a94bbacca7240c221e4754ab21d560eb5943
namankumar818 <80568028+namankumar818@users.noreply.github.com>
2024-08-20 14:17:15 +0530

    Update Login.js

--- Login.js
+++ Login.js
@@ -10,6 +10,27 @@ const Login = (props) => {
   const navigate = useNavigate()
 
   const onButtonClick = () => {
+     setEmailError('')
+  setPasswordError('')
+  if ('' === email) {
+    setEmailError('Please enter your email')
+    return
+  }
+
+  if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+    setEmailError('Please enter a valid email')
+    return
+  }
+
+  if ('' === password) {
+    setPasswordError('Please enter a password')
+    return
+  }
+
+  if (password.length < 7) {
+    setPasswordError('The password must be 8 characters or longer')
+    return
+  }
   }
 
   return (
